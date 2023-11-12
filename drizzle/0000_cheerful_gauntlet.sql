23f581798ef1246f49cf29ddf1e5c44d6cff66e0
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-11-11 23:31:03 -0500

    I AM STILL DEBUGGING :(

--- drizzle/0000_cheerful_gauntlet.sql
+++ drizzle/0000_cheerful_gauntlet.sql
@@ -1,3 +1,10 @@
+CREATE TABLE IF NOT EXISTS "admin" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"username" varchar(256) NOT NULL,
+	"password" varchar(256) NOT NULL,
+	CONSTRAINT "admin_username_unique" UNIQUE("username")
+);
+--> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "blogs" (
 	"slug" varchar(256) PRIMARY KEY NOT NULL,
 	"date" bigint NOT NULL,
