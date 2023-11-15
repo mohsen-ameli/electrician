597f1d8c8013f8d582e20bb0b46545f8e3fca9a7
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-11-14 20:54:25 -0500

    was it a syntax error after all?

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,27 +1,12 @@
 import { db } from "@/db/drizzle-db"
-import { admin, users } from "@/db/schema"
-import { hash } from "bcryptjs"
-import { sql } from "drizzle-orm"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest) {
-  // const blogs = await db.query.blogs.findMany()
-  // const { searchParams } = new URL(req.url)
-  // const username = searchParams.get("username")
-  // const blogs = await db.execute(sql`CREATE TABLE IF NOT EXISTS "admin" (
-  //   "id" serial PRIMARY KEY NOT NULL,
-  //   "username" varchar(256) NOT NULL,
-  //   "password" varchar(256) NOT NULL,
-  //   CONSTRAINT "admin_username_unique" UNIQUE("username")
-  // );`)
-  // await db.insert(users).values({
-  //   username: ".",
-  //   password: await hash(".", 10),
-  // })
-  // console.log(username)
-  // const a = await db.execute(
-  //   sql`SELECT * FROM admin WHERE username = ${username};`
-  // )
+  const { searchParams } = new URL(req.url)
+  const username = searchParams.get("username") as string
+  const user = await db.query.admin.findFirst({
+    where: (admin, { eq }) => eq(admin.username, username),
+  })
 
-  return NextResponse.json({ users: await db.query.users.findMany() })
+  return NextResponse.json(user)
 }
--- src/app/api/test3/route.ts
+++ src/app/api/test3/route.ts
@@ -1,14 +1,11 @@
 import { db } from "@/db/drizzle-db"
-import { users } from "@/db/schema"
-import { eq, sql } from "drizzle-orm"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url)
   const username = searchParams.get("username") as string
-  console.log("TEST 2222222222222222222222222222", username)
   const user = await db.query.users.findFirst({
-    where: eq(users.username, username),
+    where: (users, { eq }) => eq(users.username, username),
   })
 
   return NextResponse.json(user)
