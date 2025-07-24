97061afad2d5acbdfa32e19b65eb56649acb6f8d
mohsen-ameli <mohsen.ameli83@gmail.com>
2025-07-23 22:17:54 -0400

    added layout back into /app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "../globals.css"
+import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
